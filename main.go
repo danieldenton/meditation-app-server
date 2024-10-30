2854a7e7dc64a5ff1c9cdd0c00a0efabfca03efb
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-10-30 14:49:02 -0700

    connStr created

--- main.go
+++ main.go
@@ -1,4 +1,21 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 func main() {
+	postgres_secret := os.Getenv("POSTGRES_SECRET")
+	connStr := fmt.Sprintf("postgres://postgres:%s@localhost:5432/meditaiondb?sslmode=disable", postgres_secret)
 }
