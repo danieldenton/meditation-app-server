c7fbe4b4bba3c205daedfa1294a5185f33de4bf9
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-10-30 16:49:23 -0700

    createdTable and InsertUser created and working

--- main.go
+++ main.go
@@ -10,6 +10,13 @@ import (
 	_ "github.com/lib/pq"
 )
 
+type User struct {
+	Uid       string
+	FirstName string
+	LastName  string
+	Email     string
+}
+
 func init() {
 	err := godotenv.Load()
 	if err != nil {
@@ -34,4 +41,43 @@ func main() {
 		log.Fatal(err)
 	}
 
+	createProductionTable(db)
+
+	user := User{"HFbwu4jiaLMxEjC8nMarOjJ55Ou2", "Daniel", "Denton", "danieldentondev@gmail.com"}
+	pk := insertUser(db, user)
+
+	fmt.Println("User created with id:", pk)
+}
+
+func createProductionTable(db *sql.DB) {
+	//ID
+	//first name
+	//last name
+	//email
+	//created_at
+	query := `CREATE TABLE IF NOT EXISTS users (
+		id SERIAL PRIMARY KEY,
+		uid VARCHAR(100) NOT NULL,
+		first_name VARCHAR(100) NOT NULL,
+		last_name VARCHAR(100) NOT NULL,
+		email VARCHAR(100) NOT NULL,
+		created_at TIMESTAMP DEFAULT NOW()
+		)`
+
+	_, err := db.Exec(query)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func insertUser(db *sql.DB, user User) int {
+	query := `INSERT INTO users (uid, first_name, last_name, email) VALUES ($1, $2, $3, $4) RETURNING id`
+
+	var pk int
+	err := db.QueryRow(query, user.Uid, user.FirstName, user.LastName, user.Email).Scan(&pk)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return pk
 }
