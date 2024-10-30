337bb5f6fb2f67f386e53ec2d6d165705fce4533
Daniel Denton <96963270+danieldenton@users.noreply.github.com>
2024-10-30 15:40:58 -0700

    Go connecting to Postgres

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 func init() {
@@ -16,6 +18,20 @@ func init() {
 }
 
 func main() {
-	postgres_secret := os.Getenv("POSTGRES_SECRET")
-	connStr := fmt.Sprintf("postgres://postgres:%s@localhost:5432/meditaiondb?sslmode=disable", postgres_secret)
+	postgres_secret := os.Getenv("POSTGRES_PASSWORD")
+
+	connStr := fmt.Sprintf("postgres://postgres:%s@localhost:5432/meditationdb?sslmode=disable", postgres_secret)
+
+	db, err := sql.Open("postgres", connStr)
+
+	defer db.Close()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if err = db.Ping(); err != nil {
+		log.Fatal(err)
+	}
+
 }
